74e8306372f60ee992ddcde9ce552a003e838903
seyeadamaUASZ <a.seye3777@gmail.com>
2023-09-02 00:46:08 +0100

    add flyway for migration

--- src/main/java/com/sid/gl/models/Book.java
+++ src/main/java/com/sid/gl/models/Book.java
@@ -15,6 +15,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String title;
     private String isbn;
     @Lob
